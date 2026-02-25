2b4f36392ee9d50d8a623473e13fe9cd34f15950
STS-Engineer <administration.sts@avocarbon.com>
2026-02-25 10:38:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cron = require("node-cron");
 const app = express();   
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors());    
 app.use(bodyParser.json());   
 app.use(bodyParser.urlencoded({ extended: true }));
 
