79e02af6eccfd894fd0036509ad837f444ebb545
STS-Engineer <administration.sts@avocarbon.com>
2026-02-25 14:43:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3106,7 +3106,7 @@ cron.schedule(
 // ---------- Schedule Weekly Reports  to send it for each responsible  ----------
 let reportCronRunning = false;
 cron.schedule(
-  "04 13 * * *", // Every MOnday at 9:00 AM
+  "45 14 * * *", // Every MOnday at 9:00 AM
   async () => {
     if (reportCronRunning) {
       console.log("⏭️ Weekly report cron already running, skipping...");
