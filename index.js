74efa67a9e523ba0156ee5c6dde97bfad2569013
STS-Engineer <administration.sts@avocarbon.com>
2026-06-03 21:10:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32055,7 +32055,7 @@ console.log("Response:", info.response);
 
 // ---------- Cron: weekly reports ----------
 let reportCronRunning = false;
-cron.schedule("07 21 * * *", async () => {
+cron.schedule("13 21 * * *", async () => {
   const lockId = "weekly_kpi_report_job";
   const lock = await acquireJobLock(lockId);
   if (!lock.acquired) return;
